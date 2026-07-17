66aac7686aadbd857309debf5e251f78b450ec17
Kumar_Timalsina <xmp19@txstate.edu>
2026-07-16 19:56:47 -0500

    fix: update workflow

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Static export for GitHub Pages
 };
 
 export default nextConfig;
